d304907732eb6cb19aa9c5129ea3a7071379f343
Mona Lisa <Kozu-Ikoi@github.com>
2020-03-20 13:39:54 +0900

    Updates

---
+++ precache-manifest.f156402b871003ed9211de92d9d2aa9f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5b6bdb953b750467a1f78644834c505",
+    "url": "/project/index.html"
+  },
+  {
+    "revision": "2818496074369b973467",
+    "url": "/project/static/css/main.00dcdcf0.chunk.css"
+  },
+  {
+    "revision": "d58db007adc64ea5cbca",
+    "url": "/project/static/js/2.7517d2a7.chunk.js"
+  },
+  {
+    "revision": "685e179eab013b665e788e7cfaf87ff5",
+    "url": "/project/static/js/2.7517d2a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2818496074369b973467",
+    "url": "/project/static/js/main.610f4be1.chunk.js"
+  },
+  {
+    "revision": "7d112ba2351fcefa2956",
+    "url": "/project/static/js/runtime-main.3ee44703.js"
+  }
+]);
